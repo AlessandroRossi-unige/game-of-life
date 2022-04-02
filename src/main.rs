e2f665d02b616fd31c92d05e98ce919d041d5c9a
Alessandro Rossi <S4641902@studenti.unige.it>
2022-04-02 11:17:18 +0200

    Working window

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-use std::io;
+use bevy::prelude::*;
+
+struct WinSize {
+    w: f32,
+    h: f32,
+}
+
 
 fn main() {
-    println!("Hello!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(1., 1., 1.)))
+        .insert_resource(WindowDescriptor {
+            title: "Conway's Game Of Life!".to_string(),
+            width: 1024.,
+            height: 1024.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run()
+}
 
-    let mut guess = String::new();
+fn setup(
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("");
+) {
+
 }
 
